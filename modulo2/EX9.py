be91daef83071568bb0ac297697fe9ea9db01f05
Fernando Costa <fernando.costa@sec4you.com.br>
2019-07-19 12:20:50 -0300

    :star: new(EX9): media de 10 numeros

---
+++ modulo2/EX9.py
@@ -0,0 +1,11 @@
+'''
+Calculo da media de dez numeros inteiros
+'''
+
+n = 1
+soma = 0
+while n <= 10:
+    x = int(input('Digite o %d numero:' % n))
+    soma = soma+x
+    n += 1
+print('Media: %5.2f', (soma/10))
