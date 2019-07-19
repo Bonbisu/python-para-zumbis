5f1f6e43271b33fe725e20c30513123a95c95319
Fernando Costa <fernando.costa@sec4you.com.br>
2019-07-19 11:52:31 -0300

    :star: new(EX8): soma de 10 inteiros

--- modulo2/EX8.py
+++ modulo2/EX8.py
@@ -1,7 +1,14 @@
-# Calculo da sima de dez numeros inteiros
+'''
+Calculo da soma de dez numeros inteiros
 
-n = 1
-soma = 0
+A diferença entre um contador e um acumilador é que
+nos contadores o valor adicionado é constante e,
+nos acumuladores, variável.
+
+'''
+
+n = 1  # contador
+soma = 0  # acumulador
 while n <= 10:
     x = int(input('Digite o %d numero:' % n))
     soma = soma+x
