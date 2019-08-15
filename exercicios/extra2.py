f6862e81b0a2e86a8497a91dd32bd3e058390db9
bonbisu <fernando.costa30@fatec.sp.gov.br>
2019-08-15 02:31:24 -0300

    :star: new(exercicio) google 13

---
+++ exercicios/extra2.py
@@ -0,0 +1,27 @@
+'''
+Lista Extra (não valem ponto). Entrega opcional.
+Questão A. Problema de Josephus. Imagine que temos n pessoas dispostas em círculo. Suponha que as
+pessoas estão numeradas 1 a n no sentido horário. Começando com a pessoa de número 1, percorra o
+círculo no sentido horário e elimine cada m-ésima pessoa enquanto o círculo tiver duas ou mais pessoas.
+Qual o número do sobrevivente? Teste para n = 50 e m = 3, reposta 11.
+Questão B. Implemente o crivo de Eratóstenes: http://pt.wikipedia.org/wiki/Crivo_de_Eratóstenes
+Questão C. Verifique para que serve a palavra reservada yield e dpois resolva o problema 2 do Euler
+Project: http://projecteuler.net/problem=2.
+Questão D. (Enade 2011) No livro “O Homem que Calculava”, de Malba Tahan, um personagem desejava
+ganhar os grãos de trigos que fossem distribuídos sobre um tabuleiro de xadrez do seguinte modo: um
+grão na primeira casa do tabuleiro, o dobro (2) na segunda, novamente o dobro (4) na terceira, outra vez o
+dobro (8) na quarta, e assim por diante, até a sexagésima quarta casa do tabuleiro. Faça um algoritmo que
+calcule a quantidade total de grãos de trigos necessários para realizar esta distribuição.
+Questão E (OBI ensino médio). São dados N azulejos de dimensões 10cm × 10cm. Com eles, você deve
+montar um conjunto de quadrados (com espessura de um azulejo) de modo a utilizar TODOS os azulejos
+dados. Inicialmente você deve montar o maior quadrado possível com os azulejos dados; então, com os
+azulejos que sobraram, você deve montar o maior quadrado possível, e assim sucessivamente. Por
+exemplo, se forem dados 31 azulejos, o conjunto montado terá quatro quadrados, conforme ilustra a
+figura abaixo. Faça uma função que recebe o número de azulejos e calcule quais quadrados são montados.
+Suponha que não teremos mais que 400 azulejos e teste para 76, 290 e 347 azulejos. No exemplo abaixo a
+resposta seria:
+1 quadrado(s) de lado 5
+1 quadrado(s) de lado 2
+2 quadrado(s) de lado 1
+
+'''
